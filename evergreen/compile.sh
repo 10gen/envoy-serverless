aee8ba01568a5217f0733f43ed1d96ea9901112b
Siyuan Zhou <siyuan.zhou@mongodb.com>
2022-12-08 05:12:20 +0000

    Initial version of Evergreen config.

---
+++ evergreen/compile.sh
@@ -0,0 +1,9 @@
+ #!/bin/bash
+set -euo pipefail
+
+sudo yum install -y podman fuse-overlayfs conmon || sudo dnf install -y podman fuse-overlayfs conmon
+export REPODIR=/etc/envoy-serverless
+export SRCDIR=$(pwd)/src
+# Make build directory
+mkdir -p ./build
+sudo podman --root="$(pwd)/containers" run -v "$SRCDIR:$REPODIR" -v "$(pwd)/build:/build" docker.io/siyuanzhou/envoy-serverless-centos7 $REPODIR/evergreen/7.sh
