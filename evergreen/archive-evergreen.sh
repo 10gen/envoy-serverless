73dfcf3d59ef74844de9d63ea909e3c20a80b0e9
Siyuan Zhou <siyuan.zhou@mongodb.com>
2023-01-02 04:39:31 +0000

    Add archive

---
+++ evergreen/archive-evergreen.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+set -xeuo pipefail
+
+SRCDIR=$(pwd)/envoy-serverless
+cd $SRCDIR
+# Generate version from the most recent base tag, e.g. v1.21.3-4-gdbdcfa34cf
+# This example is based on the most recent tag "v1.21.3" and has "4" commits on top of that,
+# then it appends the "g" prefix (for git) and an abbreviated object name for the commit.
+# --dirty: If the working tree has local modification "-dirty" is appended to it.
+# --always: Show uniquely abbreviated commit object as fallback when no tag is found.
+VERSION=$(git describe --tags --dirty --always --match 'v[0-9]*')
+cd -
+
+ARCHIVE_DIR=envoy-serverless-${VERSION}.rhel7.amd64
+mkdir -p ${ARCHIVE_DIR}
+# Only include the envoy binary and the hot-restarter. The start script and the static config should
+# be prepared by the agent.
+cp build/execroot/envoy/bazel-out/k8-opt/bin/source/exe/envoy-static ${ARCHIVE_DIR}/envoy-serverless
+cp ${SRCDIR}/restarter/hot-restarter.py ${ARCHIVE_DIR}
+tar -zcvf ${ARCHIVE_DIR}.tar.gz ${ARCHIVE_DIR}
