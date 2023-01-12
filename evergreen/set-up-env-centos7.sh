9d0faec061e13868ea8a4d07245cc1a4ce74b6d9
Siyuan Zhou <siyuan.zhou@mongodb.com>
2023-01-12 00:46:59 +0000

    ARM compile success.

--- evergreen/set-up-env-centos7.sh
+++ evergreen/set-up-env-centos7.sh
@@ -3,15 +3,15 @@
 # This script runs in a container built from Dockerfile-centos7.
 # It assumes the workspace directory is mounted on /etc/envoy-serverless and the build directory is optionally mounted on /build
 
-set -xeuo pipefail
+# set -xeuo pipefail
 
 # Use MongoDB toolchain installed in the docker image.
 export LD_LIBRARY_PATH="/opt/mongodbtoolchain/v4/lib"
-export CC=/opt/mongodbtoolchain/v4/bin/clang
-export CXX=/opt/mongodbtoolchain/v4/bin/clang++
+export bazel_flags="--config=linux --config=clang -c opt --action_env=PATH --verbose_failures -k"
 
-export bazel_flags="--config=linux --config=clang"
-export bazel_flags="${bazel_flags} -c opt --action_env=PATH --action_env=CC=${CC} --action_env=CXX=${CXX} --action_env=LD_LIBRARY_PATH --verbose_failures -k"
+# export CC=/opt/mongodbtoolchain/v4/bin/clang
+# export CXX=/opt/mongodbtoolchain/v4/bin/clang++
+# export bazel_flags="${bazel_flags} --action_env=CC=${CC} --action_env=CXX=${CXX} --action_env=LD_LIBRARY_PATH"
 
 # Set the bazel repository cache and output base to /build, so that they are not on the docker overlay volume.
 export BUILD_DIR=${BUILD_DIR:-/build}
