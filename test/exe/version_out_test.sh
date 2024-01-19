528e23cf27d0677bed257dbefdba000a454bf529
Tristan Wedderburn <tristan.wedderburn@mongodb.com>
2024-01-19 15:38:33 -0500

    CLOUDP-223402: Failed Task: serverless-proxy-envoy_test/al2-aarch64 (#133)
    * include prefix in version check
    
    Signed-off-by: Tristan Wedderburn <tristan.wedderburn@mongodb.com>
    
    * add comment with expected values
    
    Signed-off-by: Tristan Wedderburn <tristan.wedderburn@mongodb.com>
    
    ---------
    
    Signed-off-by: Tristan Wedderburn <tristan.wedderburn@mongodb.com>

--- test/exe/version_out_test.sh
+++ test/exe/version_out_test.sh
@@ -7,6 +7,17 @@ export LC_ALL=C
 
 ENVOY_BIN="${TEST_SRCDIR}/envoy/source/exe/envoy-static"
 
+# Note: The output of the --version command differs based on the presence of a RELEASE_VERSION file during bazel workspace status generation.
+# If the file is present, the --version command output will include the content of the file as a prefix to the text after the first '/'.
+# For example:
+# bazel-bin/envoy  version: 2f7ac770075de46ce960f4da95b40bd49834dcb1/20240104-1.26.3-65-g2f7ac77/Modified/DEBUG/BoringSSL
+# where 20240104 is taken from the RELEASE_VERSION file.
+# If RELEASE_VERSION was not present, the --version command output would be:
+# bazel-bin/envoy  version: 2f7ac770075de46ce960f4da95b40bd49834dcb1/1.26.3-65-g2f7ac77/Modified/DEBUG/BoringSSL
+# This prefix is optionally captured by the second regex capture group below.
+
+# Example expected COMMIT: 2f7ac770075de46ce960f4da95b40bd49834dcb1
+
 COMMIT=$(${ENVOY_BIN} --version | \
   sed -n -E 's/.*version: ([0-9a-f]{40})\/([0-9]+-)?([0-9]+\.[0-9]+\.[0-9]+)(-[a-zA-Z0-9_\-]+)?\/(Clean|Modified)\/(RELEASE|DEBUG)\/([a-zA-Z-]+)$/\1/p')
 
@@ -17,8 +28,10 @@ if [[ "${COMMIT}" != "${EXPECTED}" ]]; then
   exit 1
 fi
 
+# Example expected VERSION: 20240104-1.26.3-65-g2f7ac77
+
 VERSION=$(${ENVOY_BIN} --version | \
-  sed -n -E 's/.*version: ([0-9a-f]{40})\/([0-9]+-)?([0-9]+\.[0-9]+\.[0-9]+)(-[a-zA-Z0-9_\-]+)?\/(Clean|Modified)\/(RELEASE|DEBUG)\/([a-zA-Z-]+)$/\3\4/p')
+  sed -n -E 's/.*version: ([0-9a-f]{40})\/([0-9]+-)?([0-9]+\.[0-9]+\.[0-9]+)(-[a-zA-Z0-9_\-]+)?\/(Clean|Modified)\/(RELEASE|DEBUG)\/([a-zA-Z-]+)$/\2\3\4/p')
 
 EXPECTED=$(cat "${TEST_SRCDIR}/envoy/bazel/git_version.txt")
 
